d125b34fee93a1caaa9934c0d9b7ada463526b84
Russ Pelton <iamkrashenbern@yahoo.com>
2022-10-26 11:54:50 -0500

    Got a post to display in /posts after login.

---
+++ src/components/posts/Posts.js
@@ -0,0 +1,39 @@
+import { useRef, useState, useEffect } from "react"
+import { Link, useNavigate } from "react-router-dom"
+
+export const Posts = () => {
+    const [posts, setPosts] = useState([])
+
+    useEffect(
+        () => {
+            return fetch(`http://localhost:8088/posts`)
+                .then(response => response.json())
+                .then((postArray) => {
+                    setPosts(postArray)
+                })
+        }, [])
+
+    const getAllPosts = () => {
+        return fetch(`http://localhost:8088/posts`)
+            .then(response => response.json())
+            .then((postArray) => {
+                setPosts(postArray)
+            })
+    }
+    return <>
+        <h1>Posts:</h1>
+        <article className="postList">
+            {
+                posts.map(
+                    (post) => {
+                        return <section className="postContainer" key={`post--${post.id}`}>
+                            <header>{post.title} is {post.approved} </header>
+                            <body> It's a {post.category.id} and {post.content} </body>
+                            <footer> by {post.user_id} on {post.publication_date} </footer>
+                        </section>
+                    }
+                )
+            }
+        </article>
+    </>
+}
--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -2,15 +2,16 @@ import { Route, Routes } from "react-router-dom"
 import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
 import { Authorized } from "./Authorized"
+import { Posts } from "../components/posts/Posts"
 
 export const ApplicationViews = ({ token, setToken }) => {
   return <>
     <Routes>
-      <Route path="/login" element={<Login setToken={setToken} />}  />
-      <Route path="/register" element={<Register setToken={setToken} />}  />
+      <Route path="/login" element={<Login setToken={setToken} />} />
+      <Route path="/register" element={<Register setToken={setToken} />} />
       <Route element={<Authorized token={token} />}>
         {/* Add Routes here */}
-        
+        <Route path="/posts" element={<Posts setToken={setToken} />} />
       </Route>
     </Routes>
   </>
