382b77de1253a2585acab699d81890d7c6899e63
Nathenial Buckley <nathenialbuckley98@gmail.com>
2022-10-27 14:42:00 -0500

    Commiting to merge

--- src/components/tags/TagsList.js
+++ src/components/tags/TagsList.js
@@ -13,14 +13,30 @@ export const TagsList = () => {
             })
     }, [])
 
+    const willDelete = (tags) => {
+        const copy = {
+            label: tags.label
+        }
+        return fetch(`http://localhost:8088/tags/${tags.id}`, {
+            method: "DELETE",
+        })
+            .then(response => response.json())
+            .then(() => {
+                fetch(`http://localhost:8088/tags`)
+                    .then(response => response.json())
+                    .then((tags) => {
+                        setTags(tags)
+                    })
+            })
+    }
+
     return <article className="tagList">
         {
 
             tags.map(tag => <Tags
                 id={tag.id}
                 label={tag.label}
-            />
-            )
+            />)
 
         }
     </article>
