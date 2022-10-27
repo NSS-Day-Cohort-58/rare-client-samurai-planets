4a064c5dd9ff5cc8e82a599114d05482151baebc
Nathenial Buckley <nathenialbuckley98@gmail.com>
2022-10-27 10:52:47 -0500

    Added some css to the navbar and posts

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,5 +1,6 @@
 import { useRef, useState, useEffect } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import "./Posts.css"
 
 export const Posts = () => {
     const [posts, setPosts] = useState([])
