57f4c7d4a1a57de6e9ba5c6db7e47a545a2caef9
Mckenzie Rondeau <mckenzierondeau@gmail.com>
2022-11-22 13:53:32 -0600

    linked commentlist to button on post details

--- src/components/posts/PostDetails.js
+++ src/components/posts/PostDetails.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { Link } from "react-router-dom"
 import { getPost } from "../../managers/PostManager"
 import "./Posts.css"
@@ -7,6 +7,7 @@ import "./Posts.css"
 export const PostDetails = () => {
     const { postId } = useParams()
     const [post, updatePost] = useState()
+    const navigate = useNavigate()
 
     useEffect(() => {
         getPost(postId).then(updatePost)
@@ -20,7 +21,10 @@ export const PostDetails = () => {
         <div className="post_categories" > Category: {post?.category?.label}</div>
         <div className="post_date" > Date Created: {post?.date}</div>
         <div className="post_author" ><h1>By: <Link to={`/users/${post?.author}`}>{post?.author?.user?.first_name} {post?.author?.user?.last_name} </Link> </h1> </div>
-        <button className="post_button">Comments</button>
+        <button className="post_button"
+        onClick={() => {
+            navigate({ pathname: `/comments` })
+        }}>Comments</button>
         <div> </div>
         <footer className="post__footer"></footer>
     </section>
